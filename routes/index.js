913c77b056e8fa8051a39bef7cf77cc6a049d4bf
kathyavini <kathyavini@gmail.com>
2022-09-27 13:25:05 -0700

    Add views and routes for messages and form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'The Mini Message Board', messages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'The Mini Message Board' });
+});
+
+router.post('/new', (req, res, next) => {
+  res.redirect('/');
 });
 
 module.exports = router;
